450ad07ae9100e69b8757b0e7833413f1af73b54
Deukhwa Jeong <danjeong@deukhwas-mbp-2.home>
2023-08-23 10:47:00 -0400

    add contact page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./scenes/home";
 import { SelectedPage } from "@/shared/types";
 import Benefits from "./scenes/benefits";
 import OurClasses from "./scenes/classes";
+import ContactUs from "./scenes/contact";
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home,
@@ -32,6 +33,7 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </div>
   );
 }
