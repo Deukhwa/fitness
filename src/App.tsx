45b84a328f93757d63d2c6e9ab657072c0550b85
Deukhwa Jeong <danjeong@deukhwas-mbp-2.home>
2023-08-30 09:35:44 -0400

    add footer page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { SelectedPage } from "@/shared/types";
 import Benefits from "./scenes/benefits";
 import OurClasses from "./scenes/classes";
 import ContactUs from "./scenes/contact";
+import Footer from "./scenes/footer";
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home,
@@ -34,6 +35,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
