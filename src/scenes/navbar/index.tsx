760922362c34ada026d789ef125dfce06fee7564
Deukhwa Jeong <danjeong@deukhwas-mbp-2.home>
2023-09-01 09:14:48 -0400

    rename the image

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Link from "./Link";
-import Logo from "@/assets/logo.png";
+import Logo from "@/assets/Logo.png";
 import { SelectedPage } from "@/shared/types";
 import ActionButton from "@/shared/ActionButton";
 import useMediaQuery from "@/hooks/useMediaQuery";
